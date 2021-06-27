43559c4b51ef7f3367e22bb8e2a0a257ba9c8b25
Utshuk Dhamala <me.utshuk1001@outlook.com>
2021-06-28 03:40:00 +1000

    project complete utshuk happy with this

--- script.js
+++ script.js
@@ -5,50 +5,61 @@ const operator = document.querySelectorAll(".operator");
 const equal = document.querySelector(".equal");
 const output = document.querySelector(".output");
 const allClear = document.querySelector(".allClear");
+const decimalButton = document.querySelector(".deci");
 
+decimalButton.addEventListener("click", disableButton);
 allClear.addEventListener("click", clearScreen);
 equal.addEventListener("click", saveSecondValue);
 clear.addEventListener("click", removeElement);
 operator.forEach((op) => op.addEventListener("click", saveFirstValue));
 numberBtn.forEach((btn) => btn.addEventListener("click", display));
 
-function clearScreen() {
-  output.innerHTML = "";
-  screen.innerText = "";
-  number = "";
-}
-
 let firstValue = "";
 let operatorSign = "";
 let secondValue = "";
-function saveFirstValue() {
-  if (!screen.innerText) {
-    return;
-  }
+let sign = "";
+let number = "";
 
-  if (output.innerText) {
-    firstValue = output.innerText;
-  } else {
-    firstValue = screen.innerText;
-  }
+// * Function to disable the . Button
+function disableButton() {
+  this.disabled = true;
+}
+
+// * Function to clear the screen
+function clearScreen() {
+  output.innerHTML = ""; // clears output screen
+  screen.innerText = ""; // clears input screen
+  number = ""; // clears all the data saved
+}
+
+// * saves the first value
+function saveFirstValue() {
+  if (!screen.innerText) return; // stops when the user clicks any operator without any initial number
+
+  decimalButton.disabled = decimalButton.disabled == true ? false : false; //* unlocks the < . > button
+
+  // if output is already there then it saves the initial value as the output if not user given number
+  firstValue = output.innerText ? output.innerText : screen.innerText;
 
   operatorSign = this.innerText;
-  newNumber = `${firstValue} ${operatorSign} `;
-  number = newNumber;
-  screen.innerText = newNumber;
+  number = `${firstValue} ${operatorSign} `;
+  screen.innerText = number;
 }
 
+// **   just like the < saveFirstValue > function it saves the 2nd user input value 
 function saveSecondValue() {
-  totalWord = screen.innerText;
+  totalWord = screen.innerText; //takes the screen total text
 
-  totalWord = totalWord.replace(`${firstValue} ${operatorSign} `, "");
+  decimalButton.disabled = decimalButton.disabled == true ? false : false; //same as before unlocks the . button if its locked
+
+  totalWord = totalWord.replace(`${firstValue} ${operatorSign} `, "");  //seperates old user input to 2nd number
   secondValue = totalWord;
 
-  number = "";
-  output.innerText = operate(operatorSign, firstValue, secondValue);
+  number = "";  //clears the old data
+  output.innerText = operate(operatorSign, firstValue, secondValue);  //displays the output after calculation
 }
 
-let sign = "";
+// * Funtion removes the element when back button is pressed
 function removeElement() {
   let text = screen.innerText;
   text = text.split("");
@@ -56,31 +67,33 @@ function removeElement() {
   screen.innerText = text.join("");
 }
 
-let number = "";
+// * displays when each button is pressed to the screen
 function display(e) {
   number += this.innerText;
   screen.innerText = number;
 }
 
+// *** Function for math operation
 function add(a, b) {
-  return a + b;
+  return parseFloat((a + b).toFixed(2));
 }
 
 function subtract(a, b) {
-  return a - b;
+  return parseFloat((a - b).toFixed(2));
 }
 
 function multiply(a, b) {
-  return a * b;
+  return parseFloat((a * b).toFixed(2));
 }
 
 function divide(a, b) {
-    if (b === 0){
-        return 'ERROR'
-    }
-  return a / b;
+  if (b === 0) {
+    return "NO CAN DO BABES";
+  }
+  return parseFloat((a / b).toFixed(2));
 }
 
+// * Function to check the operator clicked and send it to respective funciton for calculation
 function operate(operator, a, b) {
   a = Number(a);
   b = Number(b);
